4f39dd9a3a80697eaa6a1d0d6bf29e7fe0a094a3
Vladimir Bilenko <vbilenko@yahoo.com>
2018-10-02 22:06:19 -0700

    REST - GETs and DELETE ready

--- src/main/java/com/vbilenko/springmvc/controller/AppController.java
+++ src/main/java/com/vbilenko/springmvc/controller/AppController.java
@@ -1,205 +1,205 @@
-package com.vbilenko.springmvc.controller;
-
-import com.vbilenko.springmvc.model.User;
-import com.vbilenko.springmvc.model.UserProfile;
-import com.vbilenko.springmvc.service.UserProfileService;
-import com.vbilenko.springmvc.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import java.util.List;
-import java.util.Locale;
-
-
-@Controller
-@RequestMapping("/")
-@SessionAttributes("roles")
-public class AppController {
-
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    UserProfileService userProfileService;
-
-    @Autowired
-    MessageSource messageSource;
-
-
-    /**
-     * This method will list all existing users.
-     */
-    @RequestMapping(value = {"/", "/list"}, method = RequestMethod.GET)
-    public String listUsers(ModelMap model) {
-
-        List<User> users = userService.findAllUsers();
-        model.addAttribute("users", users);
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "userslist";
-    }
-
-    /**
-     * This method will provide the medium to add a new user.
-     */
-    @RequestMapping(value = {"/newuser"}, method = RequestMethod.GET)
-    public String newUser(ModelMap model) {
-
-        User user = new User();
-        model.addAttribute("user", user);
-        model.addAttribute("edit", false);
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "registration";
-    }
-
-    /**
-     * This method will be called on form submission, handling POST request for
-     * saving user in database. It also validates the user input
-     */
-    @RequestMapping(value = {"/newuser"}, method = RequestMethod.POST)
-    public String saveUser(@Valid User user, BindingResult result, ModelMap model) {
-
-        if (result.hasErrors()) {
-            model.addAttribute("allUsers", userService.findAllUsers());
-            return "registration";
-        }
-
-        if (!userService.isUserSSOUnique(user.getId(), user.getSsoId())) {
-            FieldError ssoError = new FieldError("user", "ssoId", messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
-            result.addError(ssoError);
-            return "registration";
-        }
-
-        userService.saveUser(user);
-
-        model.addAttribute("user", user);
-        model.addAttribute("success", "User " + user.getFirstName() + " " + user.getLastName() + " registered successfully");
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "registrationsuccess";
-    }
-
-
-    /**
-     * This method will provide the medium to update an existing user.
-     */
-    @RequestMapping(value = {"/edit-user-{ssoId}"}, method = RequestMethod.GET)
-    public String editUser(@PathVariable String ssoId, ModelMap model) {
-        User user = userService.findBySSO(ssoId);
-        model.addAttribute("user", user);
-        model.addAttribute("edit", true);
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "registration";
-    }
-
-    /**
-     * This method will be called on form submission, handling POST request for
-     * updating user in database. It also validates the user input
-     */
-    @RequestMapping(value = {"/edit-user-{ssoId}"}, method = RequestMethod.POST)
-    public String updateUser(@Valid User user, BindingResult result,
-                             ModelMap model, @PathVariable String ssoId) {
-
-        if (result.hasErrors()) {
-            return "registration";
-        }
-
-        if (!userService.isUserSSOUnique(user.getId(), user.getSsoId())) {
-            FieldError ssoError = new FieldError("user", "ssoId", messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
-            result.addError(ssoError);
-            return "registration";
-        }
-
-        userService.updateUser(user);
-
-        model.addAttribute("success", "User " + user.getFirstName() + " " + user.getLastName() + " updated successfully");
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "registrationsuccess";
-    }
-
-
-    /**
-     * This method will delete an user by it's SSOID value.
-     */
-    @RequestMapping(value = {"/delete-user-{ssoId}"}, method = RequestMethod.GET)
-    public String deleteUser(@PathVariable String ssoId) {
-        userService.deleteUserBySSO(ssoId);
-        return "redirect:/list";
-    }
-
-
-    /**
-     * This method will provide UserProfile list to views
-     */
-    @ModelAttribute("roles")
-    public List<UserProfile> initializeProfiles() {
-        return userProfileService.findAll();
-    }
-
-    /**
-     * This method handles Access-Denied redirect.
-     */
-    @RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
-    public String accessDeniedPage(ModelMap model) {
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "accessDenied";
-    }
-
-    /**
-     * This method handles login GET requests.
-     * If users is already logged-in and tries to goto login page again, will be redirected to list page.
-     */
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginPage() {
-        return "login";
-    }
-
-    /**
-     * This method retrieves user's welcome page
-     */
-    @RequestMapping(value = "/userpage", method = RequestMethod.GET)
-    public String userPage() {
-        return "userpage";
-    }
-
-
-    /**
-     * This method handles logout requests.
-     * Toggle the handlers if you are RememberMe functionality is useless in your app.
-     */
-    @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-            SecurityContextHolder.getContext().setAuthentication(null);
-        }
-        return "redirect:/login?logout";
-    }
-
-    /**
-     * This method returns the principal[user-name] of logged-in user.
-     */
-    private String getPrincipal() {
-        String userName = null;
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        if (principal instanceof UserDetails) {
-            userName = ((UserDetails) principal).getUsername();
-        } else {
-            userName = principal.toString();
-        }
-        return userName;
-    }
-}
+//package com.vbilenko.springmvc.controller;
+//
+//import com.vbilenko.springmvc.model.User;
+//import com.vbilenko.springmvc.model.UserProfile;
+//import com.vbilenko.springmvc.service.UserProfileService;
+//import com.vbilenko.springmvc.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.MessageSource;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.FieldError;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.validation.Valid;
+//import java.util.List;
+//import java.util.Locale;
+//
+//
+//@Controller
+//@RequestMapping("/")
+//@SessionAttributes("roles")
+//public class AppController {
+//
+//    @Autowired
+//    UserService userService;
+//
+//    @Autowired
+//    UserProfileService userProfileService;
+//
+//    @Autowired
+//    MessageSource messageSource;
+//
+//
+//    /**
+//     * This method will list all existing users.
+//     */
+//    @RequestMapping(value = {"/", "/list"}, method = RequestMethod.GET)
+//    public String listUsers(ModelMap model) {
+//
+//        List<User> users = userService.findAllUsers();
+//        model.addAttribute("users", users);
+//        model.addAttribute("loggedinuser", getPrincipal());
+//        return "userslist";
+//    }
+//
+//    /**
+//     * This method will provide the medium to add a new user.
+//     */
+//    @RequestMapping(value = {"/newuser"}, method = RequestMethod.GET)
+//    public String newUser(ModelMap model) {
+//
+//        User user = new User();
+//        model.addAttribute("user", user);
+//        model.addAttribute("edit", false);
+//        model.addAttribute("loggedinuser", getPrincipal());
+//        return "registration";
+//    }
+//
+//    /**
+//     * This method will be called on form submission, handling POST request for
+//     * saving user in database. It also validates the user input
+//     */
+//    @RequestMapping(value = {"/newuser"}, method = RequestMethod.POST)
+//    public String saveUser(@Valid User user, BindingResult result, ModelMap model) {
+//
+//        if (result.hasErrors()) {
+//            model.addAttribute("allUsers", userService.findAllUsers());
+//            return "registration";
+//        }
+//
+//        if (!userService.isUserSSOUnique(user.getId(), user.getSsoId())) {
+//            FieldError ssoError = new FieldError("user", "ssoId", messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
+//            result.addError(ssoError);
+//            return "registration";
+//        }
+//
+//        userService.saveUser(user);
+//
+//        model.addAttribute("user", user);
+//        model.addAttribute("success", "User " + user.getFirstName() + " " + user.getLastName() + " registered successfully");
+//        model.addAttribute("loggedinuser", getPrincipal());
+//        return "registrationsuccess";
+//    }
+//
+//
+//    /**
+//     * This method will provide the medium to update an existing user.
+//     */
+//    @RequestMapping(value = {"/edit-user-{ssoId}"}, method = RequestMethod.GET)
+//    public String editUser(@PathVariable String ssoId, ModelMap model) {
+//        User user = userService.findBySSO(ssoId);
+//        model.addAttribute("user", user);
+//        model.addAttribute("edit", true);
+//        model.addAttribute("loggedinuser", getPrincipal());
+//        return "registration";
+//    }
+//
+//    /**
+//     * This method will be called on form submission, handling POST request for
+//     * updating user in database. It also validates the user input
+//     */
+//    @RequestMapping(value = {"/edit-user-{ssoId}"}, method = RequestMethod.POST)
+//    public String updateUser(@Valid User user, BindingResult result,
+//                             ModelMap model, @PathVariable String ssoId) {
+//
+//        if (result.hasErrors()) {
+//            return "registration";
+//        }
+//
+//        if (!userService.isUserSSOUnique(user.getId(), user.getSsoId())) {
+//            FieldError ssoError = new FieldError("user", "ssoId", messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
+//            result.addError(ssoError);
+//            return "registration";
+//        }
+//
+//        userService.updateUser(user);
+//
+//        model.addAttribute("success", "User " + user.getFirstName() + " " + user.getLastName() + " updated successfully");
+//        model.addAttribute("loggedinuser", getPrincipal());
+//        return "registrationsuccess";
+//    }
+//
+//
+//    /**
+//     * This method will delete an user by it's SSOID value.
+//     */
+//    @RequestMapping(value = {"/delete-user-{ssoId}"}, method = RequestMethod.GET)
+//    public String deleteUser(@PathVariable String ssoId) {
+//        userService.deleteUserBySSO(ssoId);
+//        return "redirect:/list";
+//    }
+//
+//
+//    /**
+//     * This method will provide UserProfile list to views
+//     */
+//    @ModelAttribute("roles")
+//    public List<UserProfile> initializeProfiles() {
+//        return userProfileService.findAll();
+//    }
+//
+//    /**
+//     * This method handles Access-Denied redirect.
+//     */
+//    @RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+//    public String accessDeniedPage(ModelMap model) {
+//        model.addAttribute("loggedinuser", getPrincipal());
+//        return "accessDenied";
+//    }
+//
+//    /**
+//     * This method handles login GET requests.
+//     * If users is already logged-in and tries to goto login page again, will be redirected to list page.
+//     */
+//    @RequestMapping(value = "/login", method = RequestMethod.GET)
+//    public String loginPage() {
+//        return "login";
+//    }
+//
+//    /**
+//     * This method retrieves user's welcome page
+//     */
+//    @RequestMapping(value = "/userpage", method = RequestMethod.GET)
+//    public String userPage() {
+//        return "userpage";
+//    }
+//
+//
+//    /**
+//     * This method handles logout requests.
+//     * Toggle the handlers if you are RememberMe functionality is useless in your app.
+//     */
+//    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+//    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if (auth != null) {
+//            new SecurityContextLogoutHandler().logout(request, response, auth);
+//            SecurityContextHolder.getContext().setAuthentication(null);
+//        }
+//        return "redirect:/login?logout";
+//    }
+//
+//    /**
+//     * This method returns the principal[user-name] of logged-in user.
+//     */
+//    private String getPrincipal() {
+//        String userName = null;
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//
+//        if (principal instanceof UserDetails) {
+//            userName = ((UserDetails) principal).getUsername();
+//        } else {
+//            userName = principal.toString();
+//        }
+//        return userName;
+//    }
+//}
